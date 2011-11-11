4faeb761840cb65cadb5fe956adf656179740d9d
Reed Lauber <reedlauber@gmail.com>
2011-11-11 13:17:49 -0500

    Added compiled assets.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     @instance = Instance.where("token = ?", @token).first
     
-    @title = @instance.name == nil ? "Untitled" : @instance.name
+    if(@instance != nil)
+      @title = @instance.name == nil ? "Untitled" : @instance.name
+    end
   end
 end
